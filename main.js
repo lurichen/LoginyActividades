9335ca6167a588e74ec08a8b7f89da8b211d5acc
jose d lopez <jose@devlabs.vc>
2018-11-02 22:40:09 -0300

    se mueve todo los de JQuery a main.js

--- main.js
+++ main.js
@@ -30,4 +30,16 @@ $(document).ready(function(){
     });
   });
 
+  /*function validate (input) {
+    if($(input).attr('type') == 'correoElectronico' || $(input).attr('contrasena') == 'correoElectronico') {
+      if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        return false;
+      }
+    } else {
+      if($(input).val().trim() == ''){
+        return false;
+      }
+    }
+  }*/
+
 });
