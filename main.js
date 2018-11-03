d3f52fe71ddd7093aa7f3b221093c110f280dc4c
jose d lopez <jose@devlabs.vc>
2018-11-02 22:00:47 -0300

    formateo, pero parace que hay unas Funciones no cerradas bien

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-
 $(document).ready(function(){
-    $('.menu li:has('ul')').click(function(e){
-     	  e.preventDefault();
 
-          if ($(this).hasClass('activado')){
-             
-        } else {
- 	    	$('.menu li ul').removeClass();
-            $('.menu li ul').removeClass('activado');
-            $('this').addClass('activado');
-            $('this').children('ul').slideDown();
- 	    }
- 
-    $('.btn-menu'.click(fuction(){
-    	$('.contenedor .menu .menu').slidetoggle();
-     $ ('.btn-menu') .menu .menu
+  $('.menu li:has('ul')').click(function(e){
+    e.preventDefault();
+
+    if ($(this).hasClass('activado')) {
+      // no hacer nada
+    } else {
+      $('.menu li ul').removeClass();
+      $('.menu li ul').removeClass('activado');
+      $('this').addClass('activado');
+      $('this').children('ul').slideDown();
+    }
+
+    $('.btn-menu').click(function() {
+      $('.contenedor .menu .menu').slidetoggle();
+      $('.btn-menu').menu.menu; // Que pasa aqui ???
     });
 
-    $(window).resize(function){
-    	  if($(document).width()>320){
-    	     $('.contenedor-menu .menu').css({'display' : 'block'});
-            }
+    $(window).resize(function() {
+      if($(document).width() > 320) {
+        $('.contenedor-menu .menu').css({'display' : 'block'});
+      }
 
-            if ($(document).width()<320{
-                $('contenedor-menu .menu').css('display' : 'none'});
-                $('.menu li ul').slideUp();
-                $('.menu li').removeclass('activado');
-            }
-    });  
-});    
-    
+      if ($(document).width() < 320) {
+        $('contenedor-menu .menu').css('display' : 'none'});
+        $('.menu li ul').slideUp();
+        $('.menu li').removeclass('activado');
+      }
+    });
+  });
+
+});
